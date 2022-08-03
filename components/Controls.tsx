ca3798984cc8e8f267a4b3247d942553e004a12b
Domenico Colandrea <Domenico.Colandrea@msci.com>
2022-08-03 01:41:33 -0400

    disable remove button if there are no butterflies

--- components/Controls.tsx
+++ components/Controls.tsx
@@ -3,12 +3,18 @@ import {
   addButterflyAction,
   removeButterflyAction,
   resetAction,
+  butterfliesSelector,
 } from "../store/selectors";
 
 const Controls = () => {
+  const butterflies = useStore(butterfliesSelector);
   const addButterfly = useStore(addButterflyAction);
   const removeButterfly = useStore(removeButterflyAction);
   const reset = useStore(resetAction);
+
+  const disabledBtn = butterflies > 0 ? "" : "cursor-not-allowed";
+  const removeBtn = `bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded mx-1 ${disabledBtn}`;
+
   return (
     <>
       <button
@@ -18,8 +24,9 @@ const Controls = () => {
         Add a Butterfly
       </button>
       <button
-        className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded mx-1"
-        onClick={() => removeButterfly(1)}
+        disabled={butterflies === 0}
+        className={removeBtn}
+        onClick={() => butterflies && removeButterfly(1)}
       >
         Remove a Butterfly
       </button>
